654bf34d91625951b7bc00f0ca8805153df777bd
Evgeny Ponkratyev <eponkratyev@tula.co>
2025-04-07 19:28:19 +0300

    Use hooks like beforeEach

--- src/app/hooks/user.spec.ts
+++ src/app/hooks/user.spec.ts
@@ -1,47 +1,34 @@
-import {expect, it} from 'vitest';
+import {beforeEach, expect, it} from 'vitest';
 import {User} from './user';
 
+const testEmail = 'test@test.com';
+let user: User;
+beforeEach(() => {
+  user = new User(testEmail);
+})
 
 it('should update the email', () => {
-  const testEmail = 'test@test.com';
   const newTestEmail = 'test2@test.com';
 
-  const user = new User(testEmail);
   user.updateEmail(newTestEmail);
 
   expect(user.email).toBe(newTestEmail);
 });
 
 it('should have an email property', () => {
-  const testEmail = 'test@test.com';
-
-  const user = new User(testEmail);
-
   expect(user).toHaveProperty('email');
 });
 
 it('should store the provided email value', () => {
-  const testEmail = 'test@test.com';
-
-  const user = new User(testEmail);
-
   expect(user.email).toBe(testEmail);
 });
 
 it('should clear the email', () => {
-  const testEmail = 'test@test.com';
-
-  const user = new User(testEmail);
   user.clearEmail();
-
   expect(user.email).toBe('');
 });
 
 it('should still have an email property after clearing the email', () => {
-  const testEmail = 'test@test.com';
-
-  const user = new User(testEmail);
   user.clearEmail();
-
   expect(user).toHaveProperty('email');
 });
