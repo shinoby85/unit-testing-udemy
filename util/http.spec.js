f0a15f6692d60c77c29b61953f8ba8778bb0dc9c
Evgeny Ponkratyev <eponkratyev@tula.co>
2025-04-08 19:17:09 +0300

    Use reject for testing

--- util/http.spec.js
+++ util/http.spec.js
@@ -4,6 +4,9 @@ import {sendDataRequest} from "./http";
 const testResponseData = {testKey: 'testData'};
 const testFetch = vi.fn((url, option) => {
   return new Promise((resolve, reject) => {
+    if (typeof option.body !== 'string') {
+      return reject('Not a string.');
+    }
     const testResponse = {
       ok: true,
       json() {
@@ -21,4 +24,15 @@ vi.stubGlobal('fetch', testFetch);
 it('should return any available response data', async () => {
   const testData = {key: 'test'};
   await expect(sendDataRequest(testData)).resolves.toEqual(testResponseData);
+});
+
+it('should convert the provided data to JSON before sending the request', async () => {
+  const testData = {key: 'test'};
+  let errorMessage;
+  try {
+    await sendDataRequest(testData)
+  } catch (error) {
+    errorMessage = error;
+  }
+  expect(errorMessage).not.toBe('Not a string.');
 });
